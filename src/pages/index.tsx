30eef5f1bc0ac9229d5c395f7cf0703b80add664
Bao Nguyen <86527891+quocbao19982009@users.noreply.github.com>
2022-07-08 10:21:54 +0300

    add random pokemonId

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,26 @@
+import { getOptionForVote } from "@/utils/getRandomPokemon";
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useEffect, useState } from "react";
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
-  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
-
+  const [firstId, secondId] = getOptionForVote();
+  const [firstPokemonId, setFirstPokemonId] = useState<null | number>(null);
+  const [secondPokemonId, setSecondPokemonId] = useState<null | number>(null);
+  // I have to use useEffect, because without useEffect, getOptionForVote will result different result on Server and on React Client -> which give error
+  useEffect(() => {
+    setFirstPokemonId(firstId!);
+    setSecondPokemonId(secondId!);
+  }, []);
   return (
     <>
       <div className="h-screen w-screen flex flex-col justify-center items-center">
         <div className="text-2xl text-center mb-2">Which Pokemon is cuter?</div>
         <div className="border p-8 rounded flex justify-between items-center max-w-2xl">
-          <div className="w-16 h-16 bg-red-200">Pokemon 1</div>
+          <div className="w-16 h-16 bg-red-200">{firstPokemonId}</div>
           <div className="p-8">VS</div>
-          <div className="w-16 h-16 bg-red-200">Pokemon 2</div>
+          <div className="w-16 h-16 bg-red-200">{secondPokemonId}</div>
         </div>
       </div>
     </>
---
+++ src/utils/getRandomPokemon.ts
@@ -0,0 +1,21 @@
+const MAX_DEX_ID = 493;
+
+export const getRandomPokemon: (skipPoekemon?: number) => number = (
+  skipPoekemon?: number
+) => {
+  const pokedexNumber = Math.floor(Math.random() * MAX_DEX_ID);
+
+  if (pokedexNumber !== skipPoekemon) {
+    //if not the same number
+    return pokedexNumber;
+  } else {
+    // If the same number than call itsself again so it will skip that number
+    return getRandomPokemon(skipPoekemon);
+  }
+};
+
+export const getOptionForVote = () => {
+  const firstId = getRandomPokemon();
+  const secondId = getRandomPokemon(firstId);
+  return [firstId, secondId];
+};
